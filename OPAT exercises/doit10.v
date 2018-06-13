9f4973fec5b34ed836aa2239a009385e0549c024
Gabriela Moreira <gabrielamoreira05@gmail.com>
2018-06-12 23:17:43 -0300

    doit10 done

--- OPAT exercises/doit10.v
+++ OPAT exercises/doit10.v
@@ -53,6 +53,14 @@ Fixpoint split {X Y : Type} (l : list (X*Y))
 (** Prove that [split] and [combine] are inverses in the following
     sense: *)
 
+Lemma list_eq : forall X (x : X) (l1 l2 : list X),
+    l1 = l2 -> x :: l1 = x :: l2.
+Proof.
+  intros X x l1 l2 eq.
+  inversion eq.
+  reflexivity.
+Qed.
+  
 Theorem combine_split : forall X Y (l : list (X * Y)) l1 l2,
   split l = (l1, l2) ->
   combine l1 l2 = l.
@@ -64,22 +72,41 @@ Proof.
    +induction l3.
     *inversion eq.
     *inversion eq. rewrite <- H0. rewrite <- H1. rewrite <- H2. rewrite <- H3.
-     apply IHl2 in eq.
-             (** [] *)
+     simpl. apply list_eq. apply IHl. reflexivity.
+Qed.
+(** [] *)
 
 (** **** Exercise: 2 stars (destruct_eqn_practice)  *)
-Theorem bool_fn_applied_thrice :
+Theorem bool_fn_applied_thrice:
   forall (f : bool -> bool) (b : bool),
   f (f (f b)) = f b.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros f b. destruct (f b) eqn:H.
+  -destruct (f true) eqn:H1.
+   +rewrite H1. reflexivity.
+   +destruct (f false) eqn:H2.
+    *reflexivity.
+    *destruct b. rewrite <- H. rewrite <- H1. reflexivity.
+     rewrite <- H2. rewrite <- H. reflexivity.
+  -destruct (f false) eqn:H1.
+   +destruct b. rewrite H. reflexivity. inversion H. rewrite <- H2. rewrite <- H1.
+    reflexivity.
+   +apply H1.
+Qed.
 (** [] *)
 
 (** **** Exercise: 3 stars (beq_nat_sym)  *)
 Theorem beq_nat_sym : forall (n m : nat),
   beq_nat n m = beq_nat m n.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n. induction n.
+  -intros m. destruct m.
+   +reflexivity.
+   +unfold beq_nat. reflexivity.
+  -intros m. destruct m.
+   +unfold beq_nat. reflexivity.
+   +simpl. apply IHn.
+Qed.
 (** [] *)
 
 (** **** Exercise: 3 stars, advanced, optional (beq_nat_sym_informal)  *)
@@ -89,7 +116,8 @@ Proof.
    Theorem: For any [nat]s [n] [m], [beq_nat n m = beq_nat m n].
 
    Proof:
-   (* FILL IN HERE *)
+   Por indução, a prova sai fácil, já que o caso base (n ou m = 0) é bem simples.
+   Assim, beq_nat 0 x é falso pra qualquer x que não seja 0 (caso base). E o único caso não base que precisa ser provado é beq_nat (S n) (S m) que é igual a beq_nat n m pela própria recursão na definição.
 *)
 (** [] *)
 
@@ -99,6 +127,17 @@ Theorem beq_nat_trans : forall n m p,
   beq_nat m p = true ->
   beq_nat n p = true.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n. induction n.
+  -intros m. induction m.
+   +intros p eq1 eq2. rewrite eq2. reflexivity.
+   +intros p eq1 eq2. inversion eq1.
+  -intros m. induction m.
+   +intros p eq1 eq2. inversion eq1.
+   +intros p eq1 eq2. inversion eq1. destruct p.
+    *inversion eq2.
+    *inversion eq2. simpl. rewrite H0. apply IHn with (m:=m).
+     rewrite H0. reflexivity.
+     rewrite H1. reflexivity.
+Qed.
 (** [] *)
 
